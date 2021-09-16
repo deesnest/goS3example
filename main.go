26562d3e0e67ab9c8ece67fe3893f685bb032836
deesnest <87329773+deesnest@users.noreply.github.com>
2021-09-16 20:23:04 +0530

    Update main.go

--- main.go
+++ main.go
@@ -15,6 +15,9 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/s3"
+	
 )
 
 const (
@@ -75,4 +78,64 @@ func handlerUpload(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprintf(w, "Successfully uploaded to %q\n", AWS_S3_BUCKET)
 	return
+}
+
+func handlerList(w http.ResponseWriter, r *http.Request) {
+
+	svc := s3.New(sess)
+	input := &s3.ListObjectsInput{
+		Bucket: aws.String(AWS_S3_BUCKET),
+	}
+
+	result, err := svc.ListObjects(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case s3.ErrCodeNoSuchBucket:
+				fmt.Println(s3.ErrCodeNoSuchBucket, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		showError(w, r, http.StatusBadRequest, "Something went wrong listing the files")
+		fmt.Println(err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html")
+
+	for _, item := range result.Contents {
+		fmt.Fprintf(w, "<li>File %s</li>", *item.Key)
+	}
+
+	return
+}
+
+func handlerDownload(w http.ResponseWriter, r *http.Request) {
+
+	// We get the name of the file on the URL
+	filename := strings.Replace(r.URL.Path, "/get/", "", 1)
+
+	f, err := os.Create(filename)
+	if err != nil {
+		showError(w, r, http.StatusBadRequest, "Something went wrong creating the local file")
+		return
+	}
+
+	// Write the contents of S3 Object to the file
+	downloader := s3manager.NewDownloader(sess)
+	_, err = downloader.Download(f, &s3.GetObjectInput{
+		Bucket: aws.String(AWS_S3_BUCKET),
+		Key:    aws.String(filename),
+	})
+	if err != nil {
+		showError(w, r, http.StatusBadRequest, "Something went wrong retrieving the file from S3")
+		return
+	}
+
+	http.ServeFile(w, r, filename)
 }
